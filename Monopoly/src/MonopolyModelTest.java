b2273755848c0bf2660288119a57a15b67e8a857
Nathan <nathanmacdiarmid@gmail.com>
2021-11-08 12:54:06 -0500

    Implemented and documented the payRent() test case.

--- Monopoly/src/MonopolyModelTest.java
+++ Monopoly/src/MonopolyModelTest.java
@@ -95,8 +95,57 @@ public class MonopolyModelTest {
 
     }
 
+    /**
+     * This test verifies that the correct amount of rent is transferred from one player to another
+     * using the payRent() method
+     *
+     * Created and documented by Nathan MacDiarmid - 101098993
+     */
     @Test
     public void payRent() {
+        int player1Money = model.getPlayer().getMoney();
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+        int player2Money = model.getPlayer().getMoney();
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // sets the owner of Mackenzie building to Player1
+        model.getBoard().getProperty(5).setOwner(model.getPlayer());
+
+        // changes turn to Player2
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // moves Player2 to Mackenzie Building
+        model.getPlayer().addPosition(5);
+
+        // verifies that Player2 paid Player1 the correct amount of rent
+        model.payRent();
+        int newPlayer1Money = player1Money + model.getBoard().getProperty(5).getRent();
+        int newPlayer2Money = player2Money - model.getBoard().getProperty(5).getRent();
+        assertEquals((newPlayer1Money - player1Money), model.getBoard().getProperty(5).getRent());
+        assertEquals((player2Money - newPlayer2Money), model.getBoard().getProperty(5).getRent());
+
+        // sets owner of Tory Building to Player2
+        model.getBoard().getProperty(8).setOwner(model.getPlayer());
+
+        // changes turn to Player1
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // moves Player1 to Tory Building
+        model.getPlayer().addPosition(8);
+
+        // verifies that Player1 paid Player2 the correct amount of rent
+        model.payRent();
+        int newerPlayer1Money = newPlayer1Money - model.getBoard().getProperty(8).getRent();
+        int newerPlayer2Money = newPlayer2Money + model.getBoard().getProperty(8).getRent();
+        assertEquals((newerPlayer2Money - newPlayer2Money), model.getBoard().getProperty(8).getRent());
+        assertEquals((newPlayer1Money - newerPlayer1Money), model.getBoard().getProperty(8).getRent());
+
+        // resets everything to original state
+        model.getBoard().getProperty(8).setOwner(null);
+        model.getBoard().getProperty(5).setOwner(null);
+        model.getPlayer().addPosition(16);
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+        model.getPlayer().addPosition(19);
 
     }
 
