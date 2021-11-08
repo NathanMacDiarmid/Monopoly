be4e6372551ebce2e5ef1aa08a068767d4c0b2e6
Nathan <nathanmacdiarmid@gmail.com>
2021-11-08 15:28:24 -0500

    Added and documented handleEmptyProperties() test case.

--- Monopoly/src/MonopolyModelTest.java
+++ Monopoly/src/MonopolyModelTest.java
@@ -31,6 +31,88 @@ public class MonopolyModelTest {
     }
 
     /**
+     * This test verifies that the handleEmptyProperties() method in MonopolyModel
+     * is returning the correct values when landing on either Go or Free Parking.
+     *
+     * Created and documented by Nathan MacDiarmid - 101098993
+     */
+    @Test
+    public void handleEmptyProperties() {
+        // verifies that Go is an empty property
+        assertTrue(model.handleEmptyProperties());
+
+        // moves Player1 to Mackenzie Building
+        model.getPlayer().addPosition(5);
+
+        // verifies that Mackenzie Building is not an empty property
+        assertFalse(model.handleEmptyProperties());
+
+        // moves Player1 to Free Parking
+        model.getPlayer().addPosition(7);
+
+        // verifies that Free Parking is an empty property
+        assertTrue(model.handleEmptyProperties());
+
+        // moves Player1 to Minto Case
+        model.getPlayer().addPosition(11);
+
+        // verifies that Minto Case is not an empty property
+        assertFalse(model.handleEmptyProperties());
+
+        // moves Player1 to Go
+        model.getPlayer().addPosition(1);
+
+        // verifies that Go is an empty property
+        assertTrue(model.handleEmptyProperties());
+    }
+
+    /**
+     * This test verifies that the correct amount of rent is transferred from one player to another
+     * using the payRent() method
+     *
+     * Created and documented by Nathan MacDiarmid - 101098993
+     */
+    @Test
+    public void payRent() {
+        int player1Money = model.getPlayer().getMoney();
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+        int player2Money = model.getPlayer().getMoney();
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // sets the owner of Mackenzie building to Player1
+        model.getBoard().getProperty(5).setOwner(model.getPlayer());
+
+        // changes turn to Player2
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // moves Player2 to Mackenzie Building
+        model.getPlayer().addPosition(5);
+
+        // verifies that Player2 paid Player1 the correct amount of rent
+        model.payRent();
+        int newPlayer1Money = player1Money + model.getBoard().getProperty(5).getRent();
+        int newPlayer2Money = player2Money - model.getBoard().getProperty(5).getRent();
+        assertEquals((newPlayer1Money - player1Money), model.getBoard().getProperty(5).getRent());
+        assertEquals((player2Money - newPlayer2Money), model.getBoard().getProperty(5).getRent());
+
+        // sets owner of Tory Building to Player2
+        model.getBoard().getProperty(8).setOwner(model.getPlayer());
+
+        // changes turn to Player1
+        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
+
+        // moves Player1 to Tory Building
+        model.getPlayer().addPosition(8);
+
+        // verifies that Player1 paid Player2 the correct amount of rent
+        model.payRent();
+        int newerPlayer1Money = newPlayer1Money - model.getBoard().getProperty(8).getRent();
+        int newerPlayer2Money = newPlayer2Money + model.getBoard().getProperty(8).getRent();
+        assertEquals((newerPlayer2Money - newPlayer2Money), model.getBoard().getProperty(8).getRent());
+        assertEquals((newPlayer1Money - newerPlayer1Money), model.getBoard().getProperty(8).getRent());
+    }
+
+    /**
      * This test verifies the correct returns of the checkProperty() method
      *
      * Created and documented by Nathan MacDiarmid - 101098993
@@ -84,59 +166,5 @@ public class MonopolyModelTest {
 
         // verifies that Player2 cannot buy the property
         assertFalse(model.checkProperty());
-    }
-
-    /**
-     * This test verifies that the correct amount of rent is transferred from one player to another
-     * using the payRent() method
-     *
-     * Created and documented by Nathan MacDiarmid - 101098993
-     */
-    @Test
-    public void payRent() {
-        int player1Money = model.getPlayer().getMoney();
-        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
-        int player2Money = model.getPlayer().getMoney();
-        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
-
-        // sets the owner of Mackenzie building to Player1
-        model.getBoard().getProperty(5).setOwner(model.getPlayer());
-
-        // changes turn to Player2
-        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
-
-        // moves Player2 to Mackenzie Building
-        model.getPlayer().addPosition(5);
-
-        // verifies that Player2 paid Player1 the correct amount of rent
-        model.payRent();
-        int newPlayer1Money = player1Money + model.getBoard().getProperty(5).getRent();
-        int newPlayer2Money = player2Money - model.getBoard().getProperty(5).getRent();
-        assertEquals((newPlayer1Money - player1Money), model.getBoard().getProperty(5).getRent());
-        assertEquals((player2Money - newPlayer2Money), model.getBoard().getProperty(5).getRent());
-
-        // sets owner of Tory Building to Player2
-        model.getBoard().getProperty(8).setOwner(model.getPlayer());
-
-        // changes turn to Player1
-        model.playerTurn = (model.playerTurn + 1) % model.getPlayers().size();
-
-        // moves Player1 to Tory Building
-        model.getPlayer().addPosition(8);
-
-        // verifies that Player1 paid Player2 the correct amount of rent
-        model.payRent();
-        int newerPlayer1Money = newPlayer1Money - model.getBoard().getProperty(8).getRent();
-        int newerPlayer2Money = newPlayer2Money + model.getBoard().getProperty(8).getRent();
-        assertEquals((newerPlayer2Money - newPlayer2Money), model.getBoard().getProperty(8).getRent());
-        assertEquals((newPlayer1Money - newerPlayer1Money), model.getBoard().getProperty(8).getRent());
-    }
-
-    @Test
-    public void getRent() {
-    }
-
-    @Test
-    public void playTurn() {
     }
 }
