f8dbc4bc2956d169dc9c5fa1e130394b1740a2fb
Nathan <nathanmacdiarmid@gmail.com>
2021-12-05 18:18:22 -0500

    Edited import method in board to transfer all boards to xml. Transfered Carleton to CarletonBoard.xml.

--- Monopoly/src/Board.java
+++ Monopoly/src/Board.java
@@ -26,6 +26,7 @@ public class Board{
     private final static int CARLETON = 0;
     private final static int CANADA = 1;
     private final static int EUROPE = 2;
+    private final static String CARLETONBOARD = "CarletonBoard.xml";
     private final static String CANADABOARD = "CanadaBoard.xml";
     private final static String EUROPEBOARD = "EuropeBoard.xml";
 
@@ -38,23 +39,7 @@ public class Board{
 
         this.properties = new ArrayList<>();
 
-        // Initializes properties depending on input type.
-        // Cost of properties is 10% of property cost
-
-        if (boardType == CARLETON) {
-            this.boardType = CARLETON;
-            initCarleton();
-        }
-
-        else if (boardType == CANADA) {
-            this.boardType = CANADA;
-            importXMLBoard(CANADABOARD);
-        }
-
-        else if (boardType == EUROPE) {
-            this.boardType = EUROPE;
-            importXMLBoard(EUROPEBOARD);
-        }
+        importXMLBoard(boardType);
 
     }
 
@@ -78,108 +63,6 @@ public class Board{
      */
     public int getBoardType() {
         return boardType;
-    }
-
-    /**
-     * This method is a private method, for internal use only.
-     * It initializes the Carleton playing Board.
-     *
-     * Created and documented by Nathan MacDiarmid - 101098993
-     */
-    private void initCarleton() {
-        Go Go = new Go("Go", 0, 0);
-
-        Property Loeb = new Property("Loeb Building", 60, 6);
-        Property Southam = new Property("Southam Hall", 60, 6);
-
-        Utilities electric = new Utilities("Electric Company", 150, 0);
-
-        Railroad Taxi = new Railroad("Taxi", 200, 25);
-
-        Property Paterson = new Property("Paterson Hall", 100, 10);
-        Property Nesbitt = new Property("Nesbitt Building", 100, 10);
-        Property Mackenzie = new Property("Makenzie Building", 120, 12);
-
-        Jail jail = new Jail("Jail", 0, 0);
-
-        Property Azreli = new Property("Azreli Theatre", 140, 14);
-        Property Steacie = new Property("Steacie Building", 140, 14);
-        Property Tory = new Property("Tory Building", 160, 16);
-
-        Railroad TunnelCart = new Railroad("Tunnel Cart", 200, 25);
-
-        Property Robertson = new Property("Robertson Hall", 180, 18);
-        Property Architecture = new Property("Architecture Building", 180, 18);
-        Property Canal = new Property("Canal Building", 200, 20);
-
-        FreeParking FreeParking = new FreeParking("Free Parking", 0, 0);
-
-        Property Glengarry = new Property("Glengarry House", 220, 22);
-        Property Lennox = new Property("Lennox House", 220, 22);
-        Property Renfrew = new Property("Renfrew House", 240, 24);
-
-        Railroad OCTranspo = new Railroad("OC Transpo", 200, 25);
-
-        Property Lanark = new Property("Lanark House", 260, 26);
-        Property Grenville = new Property("Grenville House", 260, 26);
-        Property Dundas = new Property("Dundas House", 280, 28);
-
-        GoToJail goToJail = new GoToJail("Go To Jail", 0, 0, jail);
-
-        Property Prescott = new Property("Prescott House", 300, 30);
-        Property Frontenac = new Property("Frontenac House", 300, 30);
-        Property Leeds = new Property("Leeds House", 320, 32);
-
-        Railroad OTrain = new Railroad("O Train", 200, 25);
-
-        Property Nicol = new Property("Nicol Building", 350, 35);
-
-        Utilities water = new Utilities("Water Works", 150, 0);
-
-        Property Minto = new Property("Minto CASE", 400, 40);
-
-
-        // adds properties to property list
-
-        this.properties.add(Go);
-
-        this.properties.add(Loeb);
-        this.properties.add(Southam);
-        this.properties.add(Taxi);
-        this.properties.add(Paterson);
-        this.properties.add(Nesbitt);
-        this.properties.add(Mackenzie);
-
-        this.properties.add(jail);
-
-        this.properties.add(Azreli);
-        this.properties.add(electric);
-        this.properties.add(Steacie);
-        this.properties.add(Tory);
-        this.properties.add(TunnelCart);
-        this.properties.add(Robertson);
-        this.properties.add(Architecture);
-        this.properties.add(Canal);
-
-        this.properties.add(FreeParking);
-
-        this.properties.add(Glengarry);
-        this.properties.add(Lennox);
-        this.properties.add(Renfrew);
-        this.properties.add(OCTranspo);
-        this.properties.add(Lanark);
-        this.properties.add(Grenville);
-        this.properties.add(water);
-        this.properties.add(Dundas);
-
-        this.properties.add(goToJail);
-
-        this.properties.add(Prescott);
-        this.properties.add(Frontenac);
-        this.properties.add(Leeds);
-        this.properties.add(OTrain);
-        this.properties.add(Nicol);
-        this.properties.add(Minto);
     }
 
     /**
@@ -219,14 +102,28 @@ public class Board{
 
     /**
      * Allows different types of board to be imported from XML files.
-     * @param filename the name of the XML file
+     * @param boardType the selection made by the user.
      *
      * Created and documented by Nathan MacDiarmid - 101098993
      */
-    public void importXMLBoard(String filename) {
+    private void importXMLBoard(int boardType) {
         SAXParserFactory factory = SAXParserFactory.newInstance();
+        String filename = null;
+
+        if (boardType == CARLETON) {
+            filename = CARLETONBOARD;
+        }
+
+        else if (boardType == CANADA) {
+            filename = CANADABOARD;
+        }
+
+        else if (boardType == EUROPE) {
+            filename = EUROPEBOARD;
+        }
 
         try {
+            assert filename != null;
             InputStream is = new FileInputStream(filename);
             SAXParser saxParser = factory.newSAXParser();
 
@@ -236,12 +133,7 @@ public class Board{
 
             List<Property> properties = handler.getResult();
 
-            if(filename.equals(CANADABOARD)) {
-                setBoard(CANADA, properties);
-            }
-            else if(filename.equals(EUROPEBOARD)) {
-                setBoard(EUROPE, properties);
-            }
+            setBoard(boardType, properties);
 
         } catch (ParserConfigurationException | SAXException | IOException e) {
             e.printStackTrace();
